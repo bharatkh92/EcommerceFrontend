f56704e4399bda0a69fd83fe79894c1c3d769065
bharatkh92 <bharatkh92@gmail.com>
2026-09-09 14:31:32 +0530

    fix: vercel proxy for backend

--- src/services/ecommerceApi.js
+++ src/services/ecommerceApi.js
@@ -1,9 +1,11 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
+import 'dotenv/config';
+
 
 export const ecommerceApi = createApi({
     reducerPath: 'ecommerceApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: `https://ecommercerest.onrender.com/`,
+        baseUrl: env.VITE_API_URL,
         credentials: 'include'
     }),
     tagTypes: ['Address', 'Orders', 'Cart', 'Products', 'User'],
