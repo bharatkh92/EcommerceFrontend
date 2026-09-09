7940689e24e71ac26f5c32754988f854d8e2863b
bharatkh92 <bharatkh92@gmail.com>
2026-09-09 15:06:18 +0530

    fix: base url

--- src/services/ecommerceApi.js
+++ src/services/ecommerceApi.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const ecommerceApi = createApi({
     reducerPath: 'ecommerceApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_API_URL,
+        baseUrl: "https://ecommercerest.onrender.com/",
         credentials: 'include'
     }),
     tagTypes: ['Address', 'Orders', 'Cart', 'Products', 'User'],
