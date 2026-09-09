03d26adbd4e69da4bcdb3741ff00d51552ff99ec
bharatkh92 <bharatkh92@gmail.com>
2026-09-09 11:45:45 +0530

    code: add backend url

--- src/services/ecommerceApi.js
+++ src/services/ecommerceApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const ecommerceApi = createApi({
     reducerPath: 'ecommerceApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: `http://localhost:3000`,
+        baseUrl: `https://ecommercerest.onrender.com/`,
         credentials: 'include'
     }),
     tagTypes: ['Address', 'Orders', 'Cart', 'Products', 'User'],
